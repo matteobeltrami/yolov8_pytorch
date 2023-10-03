b49dd53518349b4c1eb32ebd7870311723c7d51c
Matteo Beltrami <71525176+matteobeltrami@users.noreply.github.com>
2023-10-03 17:36:44 +0200

    Update load_params.py

--- load_params.py
+++ load_params.py
@@ -11,22 +11,28 @@ if __name__ == "__main__":
     model = YOLOv8(1,1,1)
     ii = list(model.state_dict().keys())
     our_dict = model.state_dict()
-    dat = np.load("yolov8l.npz")
-    uu = dat.files
-    oo = list(torch.load("yolov8l.pt")["model"].state_dict())
+    #dat = np.load("yolov8l.npz")
+    #uu = dat.files
+    dat = torch.load("../notebooks/yolov8l.pt")["model"].state_dict()
+    uu = dat.keys()
+
 
     if len(sys.argv) > 1:
         key = str(sys.argv[1])
         print("Counting elements with key %s." % key)
         print(count_id(ii, key))
-        print(count_id(oo, key))
-        print(count_id(uu, key))
+        #print(count_id(oo, key))
+        #print(count_id(uu, key))
 
     for i,u in enumerate(uu):
         assert our_dict[ii[i]].shape == dat[u].shape
-        our_dict[ii[i]] = torch.Tensor(dat[u] if dat[u].size != 1 else [float(dat[u])])
-
-        assert (our_dict[ii[i]].numpy() == dat[u]).all(), f"Failed at: {ii[i]}. \n Values are {dat[u]} \n {our_dict[ii[i]]}."
+        if dat[u].size != 1: 
+            tmp = torch.tensor(dat[u], dtype=dat[u].dtype)
+        else:
+            tmp = torch.tensor([dat[u]], dtype=dat[u].dtype)
+        our_dict[ii[i]] = tmp
+        print(our_dict[ii[i]].dtype , dat[u].dtype)
+        assert (our_dict[ii[i]] == dat[u]).all(), f"Failed at: {ii[i]}."# \n Values are {dat[u]} \n {our_dict[ii[i]]}."
 
     torch.save(our_dict, "yolov8l_scratch.pt")
 
